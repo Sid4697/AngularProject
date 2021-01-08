2db7f8f8aa4efb988921ee405a47465c67f7f5db
Sid4697 <siddhantvardhan007@gmail.com>
2021-01-08 18:20:42 +0530

    Second Commit

--- custom/src/app/app.module.ts
+++ custom/src/app/app.module.ts
@@ -4,15 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CustomerComponent } from './customer/customer.component';
+import { RegisterComponent } from './register/register.component';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CustomerComponent
+    CustomerComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ custom/src/app/register/register.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import {NgForm} from '@angular/forms';
+
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  submitLoginForm(form: NgForm) {
+    console.log(form.value.firstName);
+    console.log(form.value.lastName);
+    console.log(form.value.gender);
+    console.log(form.value.email);
+    console.log(form.value.address);
+    console.log(form.value.city);
+    console.log(form.value.state);
+  }
+
+}
